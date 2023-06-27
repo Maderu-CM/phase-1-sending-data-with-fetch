b88ba88d1cc57eec33f58ab87dc2894f80dcddc3
MADERU-CM <cliv.moyia@student.moringaschool.com>
2023-06-27 11:05:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(data) {
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        console.log(object);
+      })
+      .catch(function (error) {
+        alert("Error Incurred!");
+        console.log(error.message);
+      });
+  }
+  
+  // Example usage
+  const data = { name: "Clive", email: "clivemoyia@gmail.com" };
+  submitData(data);
